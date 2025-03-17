7598d7113f139d01990c769239f6465220b8d15a
Kizaaaa <themightyomegagd@gmail.com>
2025-03-17 09:43:51 +0700

    feat: Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
-    <div className="">
-      Hello World
+    <div className="text-var">
+      <div className="w-3/5 ml-[10%] mt-[10%]">
+        <h1 className="text-2xl">Selamat Datang di Sistem Seleksi Bantuan Kesejahteraan Mahasiswa</h1>
+        <h2 className="mb-8 mt-2">Kami hadir untuk membantu mahasiswa ITB yang membutuhkan dukungan finansial dalam menjalani pendidikan. Daftarkan diri Anda, lengkapi persyaratan, dan ikuti proses seleksi. Semoga bantuan ini dapat mendukung perjalanan akademik Anda!</h2>
+        <a className="bg-var hover:bg-var/90 text-white px-6 py-2 rounded-full cursor-pointer" href="/auth/login">Daftar Sekarang →</a>
+      </div>
     </div>
   );
 }
