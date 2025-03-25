ec50114932000246bed43119864a851809ef4a3c
Dzaky Satrio Nugroho <13522059@std.stei.itb.ac.id>
2025-03-26 02:16:15 +0700

    feat: add clickable button to navbar after user logged in for easier navigation

--- src/app/components/layout/navbar.tsx
+++ src/app/components/layout/navbar.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import React from 'react';
 import { useSession } from 'next-auth/react';
+import { User, LayoutDashboard } from "lucide-react"
 
 const Navbar: React.FC = () => {
 	const { data: session } = useSession();
@@ -18,10 +19,13 @@ const Navbar: React.FC = () => {
 						<h2 className='text-main'>Institut Teknologi Bandung</h2>
 					</div>
 			</Link>
-			{session? 
-				<h1 className="">{session.user?.role == "Mahasiswa"? "Profile" : "Dashboard"}</h1> :
-				<Link href="/login" className="py-2 px-5 rounded-full bg-var hover:bg-var/90 text-white">Masuk</Link>
-			}
+			{session?.user?.role === "Mahasiswa"
+			? <Link href="/student/profile" className="py-2 px-5 rounded-full bg-var hover:bg-var/90 text-white flex"><User className="h-fit mr-2" /><span>Profil</span></Link>
+			: session?.user?.role === "Pengurus_IOM"
+			? <Link href="/iom/document" className="py-2 px-5 rounded-full bg-var hover:bg-var/90 text-white flex"><LayoutDashboard className="h-fit mr-2" /><span>Dashboard</span></Link>
+			: session?.user?.role === "Admin"
+			? <Link href="/admin/account" className="py-2 px-5 rounded-full bg-var hover:bg-var/90 text-white flex"><LayoutDashboard className="h-fit mr-2" /><span>Dashboard</span></Link>
+			: <Link href="/login" className="py-2 px-5 rounded-full bg-var hover:bg-var/90 text-white">Masuk</Link>}
 		</nav>
 	);
 };
